95f830e23fd95afcc11fc18d6ab3fa1e73435ad2
DaCapdevila <dariocapdevila@gmail.com>
2018-06-13 21:31:59 -0300

    vista y layout de estados

--- resources/views/ubercommand.blade.php
+++ resources/views/ubercommand.blade.php
@@ -1,9 +1,27 @@
-@extends('layouts/master')
+@extends('layouts/status')
 
-@section('title')
-   Ubercommand
-@endsection
 
-@section('content')
-   content
+@section('content');
+  <div class="container">
+    <div class="row">
+        <div class="col-8 mx-auto">
+          <div class="card">
+
+              <form class="" action="" method="post">
+                @csrf
+                <div class="card-body bg-light">
+                  <textarea class="form-control border-0" name="body" placeholder="¿Que estás pensando?"></textarea>
+                </div>
+
+                <div class="card-footer bg-light">
+                  <button class="btn btn-primary" id="create-status">Publicar</button>
+                </div>
+              </form>
+
+          </div>
+        </div>
+    </div>
+
+  </div>
+
 @endsection
