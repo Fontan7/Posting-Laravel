064d568f4049361874e8caa97d4ebc4b09b5fe75
DaCapdevila <dariocapdevila@gmail.com>
2018-06-14 21:31:21 -0300

    Creacion de views:error/404 y 503. 503 tiene problemas >(.

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/css/app.css">
+    <title>Mi sitio</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Estamos actualizando el sitio</h1>
+        <p>Por favor intenta de nuevo más tarde</p>
+
+        <footer>Copyright ® {{ date('Y') }}</footer>
+    </div>
+    <script src="/js/app.js"></script>
+</body>
+</html>
