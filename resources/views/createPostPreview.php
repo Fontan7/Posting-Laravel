408c48aeb596e1a7db200c7a13e3b77fe9740bbc
GLJARA <36643443+GLJARA@users.noreply.github.com>
2018-06-18 19:07:38 -0300

    correcciones

---
+++ resources/views/createPostPreview.php
@@ -0,0 +1,27 @@
+@extends('layouts.master')
+
+@section('title')
+   Create Post and Preview
+@endsection
+
+@section('content')
+<div class="container-fluid">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
