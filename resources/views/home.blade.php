fe640cc19e6f272d9191ce0c012f0aaf909b4f91
DaCapdevila <dariocapdevila@gmail.com>
2018-06-30 00:07:15 -0300

    Boton me gusta en posteos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -41,7 +41,7 @@
     <div class="col-8 mx-auto">
       <div class="">
           @foreach ($posts as $post)
-          <div class="card-body d-flex flex-column card border-1 shadow-sm bg-light mb-3">
+          <div class="card-body d-flex flex-column card border-1 shadow-sm bg-light ">
             <div class="d-flex align-items-center">
               <img class="rounded-circle mr-3" width="40px" src="https://upload.wikimedia.org/wikipedia/commons/1/1e/Default-avatar.jpg">
               <div class="">
@@ -50,7 +50,12 @@
               </div>
             </div>
             <p class="card-text text-secondary" >{{ $post->text_post }}</p>
+
           </div>
+          <div class="card card-footer border-1 p-1 d-flex mb-3">
+            <i class="far fa-thumbs-up text-secondary p-2 d-flex justify-content-between align-items-center"></i>
+          </div>
+
         @endforeach
 
       </div>
@@ -58,4 +63,6 @@
   </div>
 
 </div>
+</div>
+
 @endsection
