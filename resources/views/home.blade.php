7ebff34a42c9d2b37cffe27192ef199c56ed4cb6
DaCapdevila <dariocapdevila@gmail.com>
2018-06-29 22:56:58 -0300

    Posteos realizados, revision

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -35,20 +35,27 @@
   </div>
 </div>
 <br>
-<div class="col-8 mx-auto card border-0 shadow-sm bg-light">
-  <h4 class="mx-auto">Posteos realizados</h4>
-  <div class="card-body d-flex flex-column">
-    <div class="d-flex align-items-center">
-      <img class="rounded-circle mr-3" width="40px" src="https://upload.wikimedia.org/wikipedia/commons/1/1e/Default-avatar.jpg" alt="">
+  <div class="col-10 mx-auto card border-0">
+    <h4 class="mx-auto">Posteos realizados</h4>
+
+    <div class="col-8 mx-auto">
       <div class="">
-        <h5 class="mb-1 shadow-sm">Dario Capdevila</h5>
-        <div class="small text-muted">Hace una hora</div>
+          @foreach ($posts as $post)
+          <div class="card-body d-flex flex-column card border-1 shadow-sm bg-light mb-3">
+            <div class="d-flex align-items-center">
+              <img class="rounded-circle mr-3" width="40px" src="https://upload.wikimedia.org/wikipedia/commons/1/1e/Default-avatar.jpg">
+              <div class="">
+                <h5 class="mb-1 shadow-sm">Dario Capdevila</h5>
+                <div class="small text-muted">Hace una hora</div>
+              </div>
+            </div>
+            <p class="card-text text-secondary" >{{ $post->text_post }}</p>
+          </div>
+        @endforeach
+
       </div>
     </div>
-    <div class="card-text text-secondary">
-      @foreach ($posts as $post)
-        <p>{{ $post->text_post }}</p>
-      @endforeach    </div>
   </div>
+
 </div>
 @endsection
