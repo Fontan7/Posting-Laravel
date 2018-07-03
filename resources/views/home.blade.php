5b44d256b9249fe3071fb808ad80d06bb920bf43
DaCapdevila <dariocapdevila@gmail.com>
2018-07-03 12:08:55 -0300

    nombre

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -45,7 +45,7 @@
             <div class="d-flex align-items-center">
               <img class="rounded-circle mr-3" width="40px" src="https://upload.wikimedia.org/wikipedia/commons/1/1e/Default-avatar.jpg">
               <div class="">
-                <h5 class="mb-1 shadow-sm">Dario Capdevila</h5>
+                <h5 class="mb-1 shadow-sm">{{ Auth::user()->name }}</h5>
                 <div class="small text-muted">Hace una hora</div>
               </div>
             </div>
