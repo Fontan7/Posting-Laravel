9f7d38cc515798e13e1f3522886f67a81a5ced43
DaCapdevila <dariocapdevila@gmail.com>
2018-07-04 16:12:08 -0300

    funcionaaaaaaaa

--- public/js/preview.js
+++ public/js/preview.js
@@ -2,34 +2,29 @@ function netClick(i){
 
   if (i.checked && i.value == 1) {
     document.getElementById('red').style.display='block';
-    $('#red').append('<div class="card-body d-flex flex-column card border-1 shadow-sm" id="back-post"><div class="d-flex align-items-center" ><img class="rounded-circle mr-3" width="40px" src="https://upload.wikimedia.org/wikipedia/commons/1/1e/Default-avatar.jpg"><div class=""><h5 class="mb-1 shadow-sm">Dario</h5><div class="small text-muted">Hace una hora</div></div></div><p class="card-text text-secondary">hola</p></div><div class="card card-footer border-1 p-1 d-flex mb-3"><i class="far fa-thumbs-up text-secondary p-2 d-flex justify-content-between align-items-center"></i></div>');
+    $('#red').append('<div id="facebook"><div class="card-body d-flex flex-column card border-1 shadow-sm"><div class="d-flex align-items-center" ><img class="rounded-circle mr-3" width="40px" src="https://upload.wikimedia.org/wikipedia/commons/1/1e/Default-avatar.jpg"><div class=""><h5 class="mb-1 shadow-sm">Dario - Facebook</h5><div class="small text-muted">Hace una hora</div></div></div><p class="card-text text-secondary">hola</p></div><div class="card card-footer border-1 p-1 d-flex mb-3"><i class="far fa-thumbs-up text-secondary p-2 d-flex justify-content-between align-items-center"></i></div></div>');
   } if (!i.checked && i.value == 1) {
-    $('#red').empty();
+    $('#facebook').remove();
     //document.getElementById('red').style.display='none'
   }
 
   if (i.checked && i.value == 2) {
     document.getElementById('red').style.display='block';
-    $('#red').append('<div class="card-body d-flex flex-column card border-1 shadow-sm" id="back-post"><div class="d-flex align-items-center" ><img class="rounded-circle mr-3" width="40px" src="https://upload.wikimedia.org/wikipedia/commons/1/1e/Default-avatar.jpg"><div class=""><h5 class="mb-1 shadow-sm">Dario</h5><div class="small text-muted">Hace una hora</div></div></div><p class="card-text text-secondary">hola</p></div><div class="card card-footer border-1 p-1 d-flex mb-3"><i class="far fa-thumbs-up text-secondary p-2 d-flex justify-content-between align-items-center"></i></div>');
+    $('#red').append('<div id="twitter"><div class="card-body d-flex flex-column card border-1 shadow-sm" ><div class="d-flex align-items-center" ><img class="rounded-circle mr-3" width="40px" src="https://upload.wikimedia.org/wikipedia/commons/1/1e/Default-avatar.jpg"><div class=""><h5 class="mb-1 shadow-sm">Dario - Twitter</h5><div class="small text-muted">Hace una hora</div></div></div><p class="card-text text-secondary">hola</p></div><div class="card card-footer border-1 p-1 d-flex mb-3"><i class="far fa-thumbs-up text-secondary p-2 d-flex justify-content-between align-items-center"></i></div></div>');
   } if (!i.checked && i.value == 2) {
-    $('#red').empty();
-    //document.getElementById('red').style.display='none'
+    $('#twitter').remove();
   }
 
   if (i.checked && i.value == 3) {
     document.getElementById('red').style.display='block';
-    $('#red').append('<div class="card-body d-flex flex-column card border-1 shadow-sm" id="back-post"><div class="d-flex align-items-center" ><img class="rounded-circle mr-3" width="40px" src="https://upload.wikimedia.org/wikipedia/commons/1/1e/Default-avatar.jpg"><div class=""><h5 class="mb-1 shadow-sm">Dario</h5><div class="small text-muted">Hace una hora</div></div></div><p class="card-text text-secondary">hola</p></div><div class="card card-footer border-1 p-1 d-flex mb-3"><i class="far fa-thumbs-up text-secondary p-2 d-flex justify-content-between align-items-center"></i></div>');
+    $('#red').append('<div id="instagram"><div class="card-body d-flex flex-column card border-1 shadow-sm" id="instagram"><div class="d-flex align-items-center" ><img class="rounded-circle mr-3" width="40px" src="https://upload.wikimedia.org/wikipedia/commons/1/1e/Default-avatar.jpg"><div class=""><h5 class="mb-1 shadow-sm">Dario - Instagram</h5><div class="small text-muted">Hace una hora</div></div></div><p class="card-text text-secondary">hola</p></div><div class="card card-footer border-1 p-1 d-flex mb-3"><i class="far fa-thumbs-up text-secondary p-2 d-flex justify-content-between align-items-center"></i></div></div>');
   } if (!i.checked && i.value == 3) {
-    $('#red').empty();
-    //document.getElementById('red').style.display='none'
+    $('#instagram').remove();
   }
 
   console.log(i.checked);
 }
 
-/* document.getElementByClassName('red').style.display='none';
-  html
-*/
 
 /*if (i.checked) {
   document.getElementById('red').style.display='block';
