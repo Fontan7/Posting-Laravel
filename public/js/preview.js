13fbb0d889ffe9a63abc0736c48beba6c5c5d1a6
DaCapdevila <dariocapdevila@gmail.com>
2018-07-04 13:24:08 -0300

    posteo

--- public/js/preview.js
+++ public/js/preview.js
@@ -1,10 +1,29 @@
 function netClick(i){
-  if (i.value == 1) {
+
+  if (i.checked) {
+    document.getElementById('red').style.display='block';
+    $('#red').html('<div class="card-body d-flex flex-column card border-1 shadow-sm" id="back-post"><div class="d-flex align-items-center" ><img class="rounded-circle mr-3" width="40px" src="https://upload.wikimedia.org/wikipedia/commons/1/1e/Default-avatar.jpg"><div class=""><h5 class="mb-1 shadow-sm">Dario</h5><div class="small text-muted">Hace una hora</div></div></div><p class="card-text text-secondary">hola</p></div><div class="card card-footer border-1 p-1 d-flex mb-3"><i class="far fa-thumbs-up text-secondary p-2 d-flex justify-content-between align-items-center"></i></div>');
+  } else {
+    document.getElementByClassName('red').style.display='none';
+  }
+
+  console.log(i.checked);
+}
+
+/*if (i.checked) {
+  document.getElementById('red').style.display='block';
+  $('#red').html('<div class="card-body d-flex flex-column card border-1 shadow-sm" id="back-post"><div class="d-flex align-items-center" ><img class="rounded-circle mr-3" width="40px" src="https://upload.wikimedia.org/wikipedia/commons/1/1e/Default-avatar.jpg"><div class=""><h5 class="mb-1 shadow-sm">Dario</h5><div class="small text-muted">Hace una hora</div></div></div><p class="card-text text-secondary">hola</p></div><div class="card card-footer border-1 p-1 d-flex mb-3"><i class="far fa-thumbs-up text-secondary p-2 d-flex justify-content-between align-items-center"></i></div>');
+} else {
+  document.getElementById('red').style.display='none';
+}*/
+
+
+
+/*  if (i.value == 1) {
     document.getElementById('back-post').style.background = 'white';
   } else if (i.value == 2) {
     document.getElementById('back-post').style.background = 'grey';
   } else {
     document.getElementById('back-post').style.background = 'green';
   }
-  console.log(i.value);
-}
+*/
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -39,25 +39,8 @@
     <h4 class="mx-auto">Previsualizacion de posteos</h4>
 
     <div class="col-8 mx-auto">
-      <div>
-          @foreach ($networks as $network)
-          <div class="card-body d-flex flex-column card border-1 shadow-sm" id="back-post">
-            <div class="d-flex align-items-center" >
-              <img class="rounded-circle mr-3" width="40px" src="https://upload.wikimedia.org/wikipedia/commons/1/1e/Default-avatar.jpg">
-              <div class="">
-                <h5 class="mb-1 shadow-sm"></h5>
-                <div class="small text-muted">Hace una hora</div>
-              </div>
-            </div>
-            <p class="card-text text-secondary">hola</p>
-
-          </div>
-          <div class="card card-footer border-1 p-1 d-flex mb-3">
-            <i class="far fa-thumbs-up text-secondary p-2 d-flex justify-content-between align-items-center"></i>
-          </div>
-
-        @endforeach
-
+      <div id="red">
+        
       </div>
     </div>
   </div>
