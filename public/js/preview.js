95c997df6c1e29ac4081ead839cb78739a4e2b85
DaCapdevila <dariocapdevila@gmail.com>
2018-07-04 15:56:35 -0300

    funciona mejor

--- public/js/preview.js
+++ public/js/preview.js
@@ -2,23 +2,26 @@ function netClick(i){
 
   if (i.checked && i.value == 1) {
     document.getElementById('red').style.display='block';
-    $('#red').html('<div class="card-body d-flex flex-column card border-1 shadow-sm" id="back-post"><div class="d-flex align-items-center" ><img class="rounded-circle mr-3" width="40px" src="https://upload.wikimedia.org/wikipedia/commons/1/1e/Default-avatar.jpg"><div class=""><h5 class="mb-1 shadow-sm">Dario</h5><div class="small text-muted">Hace una hora</div></div></div><p class="card-text text-secondary">hola</p></div><div class="card card-footer border-1 p-1 d-flex mb-3"><i class="far fa-thumbs-up text-secondary p-2 d-flex justify-content-between align-items-center"></i></div>');
+    $('#red').append('<div class="card-body d-flex flex-column card border-1 shadow-sm" id="back-post"><div class="d-flex align-items-center" ><img class="rounded-circle mr-3" width="40px" src="https://upload.wikimedia.org/wikipedia/commons/1/1e/Default-avatar.jpg"><div class=""><h5 class="mb-1 shadow-sm">Dario</h5><div class="small text-muted">Hace una hora</div></div></div><p class="card-text text-secondary">hola</p></div><div class="card card-footer border-1 p-1 d-flex mb-3"><i class="far fa-thumbs-up text-secondary p-2 d-flex justify-content-between align-items-center"></i></div>');
   } if (!i.checked && i.value == 1) {
-    document.getElementById('red').style.display='none'
+    $('#red').empty();
+    //document.getElementById('red').style.display='none'
   }
 
   if (i.checked && i.value == 2) {
     document.getElementById('red').style.display='block';
-    $('#red').html('<div class="card-body d-flex flex-column card border-1 shadow-sm" id="back-post"><div class="d-flex align-items-center" ><img class="rounded-circle mr-3" width="40px" src="https://upload.wikimedia.org/wikipedia/commons/1/1e/Default-avatar.jpg"><div class=""><h5 class="mb-1 shadow-sm">Dario</h5><div class="small text-muted">Hace una hora</div></div></div><p class="card-text text-secondary">hola</p></div><div class="card card-footer border-1 p-1 d-flex mb-3"><i class="far fa-thumbs-up text-secondary p-2 d-flex justify-content-between align-items-center"></i></div>');
+    $('#red').append('<div class="card-body d-flex flex-column card border-1 shadow-sm" id="back-post"><div class="d-flex align-items-center" ><img class="rounded-circle mr-3" width="40px" src="https://upload.wikimedia.org/wikipedia/commons/1/1e/Default-avatar.jpg"><div class=""><h5 class="mb-1 shadow-sm">Dario</h5><div class="small text-muted">Hace una hora</div></div></div><p class="card-text text-secondary">hola</p></div><div class="card card-footer border-1 p-1 d-flex mb-3"><i class="far fa-thumbs-up text-secondary p-2 d-flex justify-content-between align-items-center"></i></div>');
   } if (!i.checked && i.value == 2) {
-    document.getElementById('red').style.display='none'
+    $('#red').empty();
+    //document.getElementById('red').style.display='none'
   }
 
   if (i.checked && i.value == 3) {
     document.getElementById('red').style.display='block';
-    $('#red').html('<div class="card-body d-flex flex-column card border-1 shadow-sm" id="back-post"><div class="d-flex align-items-center" ><img class="rounded-circle mr-3" width="40px" src="https://upload.wikimedia.org/wikipedia/commons/1/1e/Default-avatar.jpg"><div class=""><h5 class="mb-1 shadow-sm">Dario</h5><div class="small text-muted">Hace una hora</div></div></div><p class="card-text text-secondary">hola</p></div><div class="card card-footer border-1 p-1 d-flex mb-3"><i class="far fa-thumbs-up text-secondary p-2 d-flex justify-content-between align-items-center"></i></div>');
+    $('#red').append('<div class="card-body d-flex flex-column card border-1 shadow-sm" id="back-post"><div class="d-flex align-items-center" ><img class="rounded-circle mr-3" width="40px" src="https://upload.wikimedia.org/wikipedia/commons/1/1e/Default-avatar.jpg"><div class=""><h5 class="mb-1 shadow-sm">Dario</h5><div class="small text-muted">Hace una hora</div></div></div><p class="card-text text-secondary">hola</p></div><div class="card card-footer border-1 p-1 d-flex mb-3"><i class="far fa-thumbs-up text-secondary p-2 d-flex justify-content-between align-items-center"></i></div>');
   } if (!i.checked && i.value == 3) {
-    document.getElementById('red').style.display='none'
+    $('#red').empty();
+    //document.getElementById('red').style.display='none'
   }
 
   console.log(i.checked);
