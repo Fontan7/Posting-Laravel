6394aa5f1463dc646c92534df43f99eb6e96d95c
GLJARA <36643443+GLJARA@users.noreply.github.com>
2018-07-05 13:17:41 -0300

    error en borrar cuando separe los div

--- public/js/preview.js
+++ public/js/preview.js
@@ -15,7 +15,7 @@ function netClick(i){
   var usenv = i.getAttribute('usenv');
   console.log(usenv);
 // definicion de tags a agregar en html
-  var divGral = '<div class="'.concat(i.name).concat('">'); //con nombre que llega
+  var divGral = '<div id="'.concat(i.name).concat('">'); //con nombre que llega
   var divPre = '<div id="divPre" class="card-body d-flex flex-column card border-1 shadow-sm">';
   var divEncPre = '<div id="encPre" class="d-flex align-items-center">';
   var imgUser = '<img class="rounded-circle mr-3" width="40px" src="https://upload.wikimedia.org/wikipedia/commons/1/1e/Default-avatar.jpg">';
@@ -27,7 +27,7 @@ function netClick(i){
 
   // aca se podria variar el concatenado en funcion a si se muestra primero la imagen
   // o si se muestra primero el texto
-  var textH = divGral.concat(divPre).concat(divEncPre).concat(imgUser).concat(divTit).concat('<h5>').concat(usenv).concat('- ').concat(i.name).concat('</h5>').concat(divH).concat('Hace una hora').concat(closeDiv).concat(closeDiv).concat(pPre).concat(closeDiv).concat(closeDiv).concat(footPre).concat(closeDiv);
+  var textH = divGral.concat(divPre).concat(divEncPre).concat(imgUser).concat(divTit).concat('<h5>').concat(usenv).concat('- ').concat(i.name).concat('</h5>').concat(divH).concat('Hace una hora').concat(closeDiv).concat(closeDiv).concat(pPre).concat(closeDiv).concat(footPre).concat(closeDiv);
 
 // en esta parte solamente agrega lo seteado antes cando esta marcado
   if (i.checked) {
