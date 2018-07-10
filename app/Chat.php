c12cdbc33688400aefad55307705a632b44de4fe
DaCapdevila <dariocapdevila@gmail.com>
2018-07-10 17:47:13 -0300

    Cambios en el chat

--- app/Chat.php
+++ app/Chat.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Chat extends Model
 {
-  protected $fillable = ['user_ori', 'user_dst', 'text_chat'];
+  protected $fillable = ['user_ori', 'user_dst', 'text_chat', 'created_at', 'updated_at'];
 }
