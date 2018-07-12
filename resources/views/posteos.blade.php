666117904936271b670ce71647ee8d8b352a85ce
GLJARA <36643443+GLJARA@users.noreply.github.com>
2018-07-12 09:57:59 -0300

    cambios al Posting

--- resources/views/posteos.blade.php
+++ resources/views/posteos.blade.php
@@ -8,10 +8,10 @@
 
 <div class="col-md-10 mx-auto card border-0">
   <h4 class="mx-auto my-3">Posteos realizados</h4>
+  <div class="col-md-10 mx-auto mt-1">
 
-  <div class="col-md-10 mx-auto">
     @foreach ($posts as $post)
-      <div class="card-body d-flex flex-column card border-1 shadow-sm bg-light ">
+      <div class="card-body d-flex flex-column card border-1 shadow-sm">
         <div class="d-flex align-items-center">
           <img class="rounded-circle mr-3" width="40px" src="https://upload.wikimedia.org/wikipedia/commons/1/1e/Default-avatar.jpg">
           <div class="">
@@ -20,16 +20,28 @@
           </div>
         </div>
         <p class="card-text text-secondary" >{{ $post->text_post }}</p>
-      </div>
+          <div class="imgPost">
+            @foreach ($post->images as $img)
+              <img class="img_post" src="uploads/{{$img->src}}" alt="">
+            @endforeach
+          </div>
+        </div>
       <div class="card card-footer border-1 p-1 d-flex mb-3">
-        <i class="p-2 d-flex justify-content-between align-items-center"></i>
+        <div class="nets">
+        <p class="titPre">Previsualizaciones: </p>
+        @foreach ($post->networks as $network)
+        <div >
+            <p>{{$network->description}}</p>
+        </div>
+        @endforeach
       </div>
+    </div>
     @endforeach
-  </div>
+
   <nav aria-label="Page navigation example" class="pagination justify-content-center col-md-10 mx-auto">
   {{ $posts->links() }}
   </nav>
 </div>
-
+</div>
 
 @endsection
