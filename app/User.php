d3b878f3a6089a6e903d4162b3c3b6fba6d253e1
DaCapdevila <dariocapdevila@gmail.com>
2018-07-12 19:17:52 -0300

    Auth para acceso al back

--- app/User.php
+++ app/User.php
@@ -6,6 +6,7 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 
+
 class User extends Authenticatable
 {
     use Notifiable;
@@ -37,4 +38,9 @@ class User extends Authenticatable
 {
   return $this->hasMany(Relation::class,'user_id_1','id');
 }
+
+  public function isAdmin()
+  {
+    return $this->admin;
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,16 @@
 <?php
 
-
-
 Route::get('/', function () {  //pagina de bienvenida para usuarios nuevos o deslogueados
     return view('welcome');
 });
+
 Route::get('/welcome', function () {  //hace lo mismo que la ruta anterior, solo que tambien funciona si el usuario tipea welcome en la url
     return view('welcome');
 });
+
 Route::get('/posteos', 'front\postcontroller@index');
 
-Route::get('/profile', function () {  //profile es donde el usuario puede ver su propio perfil, y cambiar su foto o nickname
-    return view('profile');
-});
-
 Auth::routes();
-
 
 Route::get('/home', 'HomeController@index')->name('home');
 
@@ -30,6 +25,12 @@ Route::get('/Contact', function () {
 // genero un route para que un administrador pueda dar de alta redes
 // a las cuales puede enviar Posting
 Route::resource('admin/networks', 'admin\NetworkController');
+// Route::get('protected', ['middleware' => ['auth', 'admin'],])
+
+Route::group(['middleware' => ['auth', 'admin']], function()
+{
+  Route::resource('admin/networks', 'admin\NetworkController');
+});
 
 // genero un route para que un administrador pueda dar de alta roles de usuarios
 Route::resource('admin/rolls', 'admin\RollController');
